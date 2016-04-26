64bf6551d0c5aa7d16ed8baaef9f79b5a8265618
Kevin Mulvey <kmulvey@mediamath.com>
2016-04-25 20:40:30 -0400

    create test db

---
+++ testdata/models.go
@@ -0,0 +1,26 @@
+package testdata
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type Profile struct {
+	gorm.Model
+	Name string
+}
+
+type User struct {
+	gorm.Model
+	Profile      Profile `gorm:"ForeignKey:ProfileRefer"` // use ProfileRefer as foreign key
+	ProfileRefer int
+}
+
+func CreateTables() {
+	db, err := gorm.Open("mysql", "root:@/gorm_test?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&User{}, &Profile{})
+
+}
---
+++ testdata/models_test.go
@@ -0,0 +1,8 @@
+package testdata
+
+import "testing"
+
+//  TestCreateTables is not a test but rather a runner
+func TestCreateTables(t *testing.T) {
+	CreateTables()
+}
