89eb0755df7e1b48181afb50a015636909ed37c6
Kevin Mulvey <kmulvey@mediamath.com>
2016-04-27 08:14:42 -0400

    linting

--- testdata/models.go
+++ testdata/models.go
@@ -1,28 +1,29 @@
 package testdata
 
 import (
+	// need this
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-type Profile struct {
+type profile struct {
 	gorm.Model
 	Name string
 }
 
-type User struct {
+type user struct {
 	gorm.Model
-	Profile      Profile `gorm:"ForeignKey:ProfileRefer"` // use ProfileRefer as foreign key
+	Profile      profile `gorm:"ForeignKey:ProfileRefer"` // use ProfileRefer as foreign key
 	ProfileRefer int
 }
 
-func CreateTables() {
+func createTables() {
 	db, err := gorm.Open("mysql", "root:@/gorm_test?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
 	//db.Model(&User{}).AddForeignKey("profile_refer", "profile(id)", "RESTRICT", "RESTRICT")
 
-	db.AutoMigrate(&User{}, &Profile{})
+	db.AutoMigrate(&user{}, &profile{})
 
 }
