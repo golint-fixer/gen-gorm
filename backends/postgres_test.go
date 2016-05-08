c1523590dce4f19b164a10674219ba820811f14d
Kevin Mulvey <kmulvey@mediamath.com>
2016-05-08 09:24:44 -0400

    update tests to reflect interface

--- backends/postgres_test.go
+++ backends/postgres_test.go
@@ -32,13 +32,14 @@ func TestGetTableInfoPostgres(t *testing.T) {
 	foreignKeys := sqlmock.NewRows([]string{"TABLE_NAME", "COLUMN_NAME", "REFERENCED_TABLE_NAME", "REFERENCED_COLUMN_NAME"}).
 		AddRow("posts", "user_id", "users", "id")
 
-	mock.ExpectQuery("SELECT table_name FROM information_schema.tables").WillReturnRows(tableRows)
-	mock.ExpectQuery("SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY FROM INFORMATION_SCHEMA.COLUMNS WHERE").WillReturnRows(colRows)
-	mock.ExpectQuery("SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY FROM INFORMATION_SCHEMA.COLUMNS WHERE").WillReturnRows(colRowsTwo)
-	mock.ExpectQuery("SELECT TABLE_NAME,COLUMN_NAME,REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE").WillReturnRows(foreignKeys)
-	m := Mysql{}
-	s := "some_schema"
-	c := ConnConfig{Schema: &s}
-	data := m.createModel(db, c)
+	mock.ExpectQuery("SELECT tablename FROM pg_catalog.pg_tables where tableowner").WillReturnRows(tableRows)
+	mock.ExpectQuery("select kc.column_name, c.data_type, tc.constraint_type from information_schema.table_constraints tc").WillReturnRows(colRows)
+	mock.ExpectQuery("select kc.column_name, c.data_type, tc.constraint_type from information_schema.table_constraints tc").WillReturnRows(colRowsTwo)
+	mock.ExpectQuery("select tc.table_name, kcu.column_name, ccu.table_name AS references_table, ccu.column_name AS references_field").WillReturnRows(foreignKeys)
+	p := Postgres{}
+	s, u := "some_schema", "adama"
+	c := ConnConfig{Schema: &s, Username: &u}
+	fmt.Println(c)
+	data := p.createModel(db, c)
 	assert.Equal(t, database, data, "should be equal")
 }
