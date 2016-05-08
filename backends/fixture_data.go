e84552bc5ae9a9ca2ee16c7918ea70ad1a982e32
Kevin Mulvey <kmulvey@mediamath.com>
2016-05-08 09:05:27 -0400

    match mysql work

--- backends/fixture_data.go
+++ backends/fixture_data.go
@@ -1,22 +1,26 @@
 package backends
 
-import "github.com/kmulvey/gen-gorm/graph"
+import (
+	"database/sql"
+
+	"github.com/kmulvey/gen-gorm/graph"
+)
 
 var users = graph.Vertex{
 	Name: "Users", HasMany: "Posts", Cols: map[string]graph.Col{
-		"Id":   {Name: "Id", Type: "int", Key: "PRI"},
-		"Name": {Name: "Name", Type: "string", Key: ""},
+		"Id":   {Name: "Id", Type: "int", Key: "PRI", MaxLen: sql.NullInt64{Int64: 0, Valid: false}},
+		"Name": {Name: "Name", Type: "string", Key: "", MaxLen: sql.NullInt64{Int64: 45, Valid: true}},
 	},
 }
-var id = graph.Col{Name: "Id", Type: "int", Key: "PRI"}
-var userID = graph.Col{Name: "UserId", Type: "int", Key: "MUL"}
+var id = graph.Col{Name: "Id", Type: "int", Key: "PRI", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
+var userID = graph.Col{Name: "UserId", Type: "int", Key: "MUL", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
 
 var database = graph.Graph{Name: "some_schema", Vertices: map[string]*graph.Vertex{
 	"Users": &users,
 	"Posts": {
 		Name: "Posts", HasMany: "", Cols: map[string]graph.Col{
 			"Id":     id,
-			"Name":   {Name: "Name", Type: "string", Key: ""},
+			"Name":   {Name: "Name", Type: "string", Key: "", MaxLen: sql.NullInt64{Int64: 45, Valid: true}},
 			"UserId": userID,
 		},
 		Edges: []graph.Edge{{DestinationTable: &users, DestinationCol: &id, OriginCol: &userID}},
