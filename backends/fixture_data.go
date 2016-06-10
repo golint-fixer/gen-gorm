351b5021ee44999c5cbfd4be101c1b8031df993a
Kevin Mulvey <kmulvey@mediamath.com>
2016-06-10 13:14:58 -0600

    fix mysql tests and remove postgres for now

--- backends/fixture_data.go
+++ backends/fixture_data.go
@@ -8,11 +8,11 @@ import (
 
 var users = graph.Vertex{
 	Name: "Users", HasMany: "Posts", Cols: map[string]graph.Col{
-		"Id":   {Name: "Id", Type: "int", Key: "PRI", MaxLen: sql.NullInt64{Int64: 0, Valid: false}},
+		"Id":   {Name: "Id", Type: "int", Key: "primary_key", MaxLen: sql.NullInt64{Int64: 0, Valid: false}},
 		"Name": {Name: "Name", Type: "string", Key: "", MaxLen: sql.NullInt64{Int64: 45, Valid: true}},
 	},
 }
-var id = graph.Col{Name: "Id", Type: "int", Key: "PRI", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
+var id = graph.Col{Name: "Id", Type: "int", Key: "primary_key", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
 var userID = graph.Col{Name: "UserId", Type: "int", Key: "MULTIPLE", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
 
 var database = graph.Graph{Name: "some_schema", Vertices: map[string]*graph.Vertex{
