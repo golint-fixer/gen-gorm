f56a2f9e2ed6aa20c8c203c8c4e7f37f2291e0c4
Kevin Mulvey <kmulvey@mediamath.com>
2016-06-11 10:29:13 -0600

    continue graph

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -9,10 +9,12 @@ import (
 
 func TestGetMeta(t *testing.T) {
 	t.Parallel()
-	var colOne = Col{Name: "Id", Type: "int", Key: "primary_key", MaxLen: sql.NullInt64{Int64: 0, Valid: false}}
-	var colTwo = Col{Name: "Name", Type: "string", Key: "", MaxLen: sql.NullInt64{Int64: 45, Valid: true}}
+	var colOne = Col{Name: "Id", Type: "int"}
+	var colTwo = Col{Name: "Id", Type: "int", Key: "primary_key", AutoInc: true}
+	var colThree = Col{Name: "Name", Type: "string", Key: "", MaxLen: sql.NullInt64{Int64: 45, Valid: true}}
 	assert.Equal(t, "", colOne.GetMeta(), "should be equal")
-	assert.Equal(t, "`gorm:size:45`", colTwo.GetMeta(), "should be equal")
+	assert.Equal(t, "`gorm:primary_key \"AUTO_INCREMENT\"`", colTwo.GetMeta(), "should be equal")
+	assert.Equal(t, "`gorm:size:45`", colThree.GetMeta(), "should be equal")
 }
 
 func TestHasEdge(t *testing.T) {
